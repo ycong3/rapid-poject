55382194b92d278b1ed85f7e04ad5ee2dfed4475
ycong3 <noq136@gmail.com>
2019-04-08 01:20:30 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,7 +2,9 @@ alien = Actor('skull')
 hero = Actor('spaceship')
 bullet = Actor('bullet')
 
-alien.bottomright = 100, 0
+import random
+
+alien.bottomright = random.randint(20, 200), 0
 hero.bottomleft = 90, 290
 bullet.bottomleft = hero.bottomleft
 WIDTH = 200
@@ -25,15 +27,21 @@ def update():
         hero.x += 5
         bullet.x += 5
 
+    if hero.left == 200:
+        bullet.right = 0
+        hero.right = 0
+    elif hero.right == 0:
+        hero.left = 200
+        bullet.left = 200
+
     if keyboard.space:
-        animate(bullet, tween='linear', duration=0.3, on_finished=None, pos=(hero.left,0))
+        animate(bullet, tween='linear', duration=0.3, on_finished=None, x=hero.x, y=0)
         clock.schedule_unique(set_bullet_normal, 0.3)
 
     if alien.bottom > bullet.top and alien.right >= bullet.left and alien.left <= bullet.right:
         set_alien_hurt()
 
         set_bullet_normal()
-
 
 
 def set_alien_hurt():
@@ -44,7 +52,7 @@ def set_alien_hurt():
 
 def set_alien_normal():
     alien.image = 'skull'
-    alien.bottom = 0
+    alien.bottomright = random.randint(20, 200), 0
 
 def set_bullet_normal():
     bullet.bottomleft = hero.bottomleft
