36a069ac2ca12a80c0e4fe80b4eddb19c68461f5
ycong3 <noq136@gmail.com>
2019-04-08 01:46:21 -0400

    SCORE not working
    Haven't figured out score

--- intro.py
+++ intro.py
@@ -2,6 +2,7 @@ alien = Actor('skull')
 hero = Actor('spaceship')
 bullet = Actor('bullet')
 
+
 import random
 
 alien.bottomright = random.randint(20, 200), 0
@@ -9,14 +10,22 @@ hero.bottomleft = 90, 290
 bullet.bottomleft = hero.bottomleft
 WIDTH = 200
 HEIGHT = 300
+score = 0
+
+def eliminate():
+    score = score + 1
 
 def draw():
+
     screen.clear()
     alien.draw()
     hero.draw()
     bullet.draw()
+    screen.draw.text(str(score), topright=(195,5), owidth=0.5, ocolor=(0,0,0), color=(255,255,255), fontsize=20)
 
 def update():
+
+    screen.draw.text(str(score), topright=(5,5), owidth=0.5, ocolor=(255,255,255), color=(255,255,255), fontsize=20)
     alien.bottom += 1
     if alien.top > HEIGHT:
         alien.bottom = 0
@@ -40,7 +49,7 @@ def update():
 
     if alien.bottom > bullet.top and alien.right >= bullet.left and alien.left <= bullet.right:
         set_alien_hurt()
-
+        # eliminate()
         set_bullet_normal()
 
 
@@ -48,6 +57,7 @@ def set_alien_hurt():
     alien.image = 'boom'
     # sounds.eep.play()
     alien.bottom -= 1
+
     clock.schedule_unique(set_alien_normal, 0.1)
 
 def set_alien_normal():
